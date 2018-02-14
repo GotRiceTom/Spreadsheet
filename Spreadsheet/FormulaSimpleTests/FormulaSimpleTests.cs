64c7c8f247712fb8ffad26e99fc3060b8f66e2ac
Eric Naegle <eric.naegle@outlook.com>
2018-02-14 11:36:34 -0700

    Professor's failed tests included.

--- Spreadsheet/FormulaSimpleTests/FormulaSimpleTests.cs
+++ Spreadsheet/FormulaSimpleTests/FormulaSimpleTests.cs
@@ -4,6 +4,7 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Formulas;
+using System.Collections.Generic;
 
 namespace FormulaTestCases
 {
@@ -46,6 +47,28 @@ namespace FormulaTestCases
         }
 
         /// <summary>
+        /// Makes sure that a zero argument constructor works like "0".
+        /// </summary>
+        [TestMethod()]
+        public void ZeroArgumentConstructor()
+        {
+            Formula f = new Formula();
+
+            if (f.ToString() != "0" && f.Evaluate(Lookup4) != 0.0)
+                Assert.Fail();
+        }
+
+        /// <summary>
+        /// Makes sure that a zero argument constructor works like "0".
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SingleNullArgumentConstructor()
+        {
+            Formula f = new Formula(null);
+        }
+
+        /// <summary>
         /// Checks that null parameters throw ArgumentNullExceptions
         /// </summary>
         [TestMethod()]
@@ -84,17 +107,6 @@ namespace FormulaTestCases
         {
             Formula f = new Formula("5 + 6", NormalizerDoesNothing, ValidatorDoesNothing);
             f.Evaluate(null);
-        }
-
-        /// <summary>
-        /// Makes sure that a zero argument constructor works like "0".
-        /// </summary>
-        [TestMethod()]
-        public void ZeroArgumentConstructor()
-        {
-            Formula f = new Formula();
-            if (f.ToString() != "0" && f.Evaluate(Lookup4) != 0.0)
-                Assert.Fail();
         }
 
         /// <summary>
@@ -334,6 +346,219 @@ namespace FormulaTestCases
         {
             Formula f = new Formula("2+3)+(5+7)-(2+5");
             Assert.AreEqual(f.Evaluate(v => 0), 20.0, 1e-6);
+        }
+
+        [TestMethod]
+        public void ZeroArg1()
+        {
+            new Formula();
+        }
+
+        [TestMethod]
+        public void ZeroArg2()
+        {
+            Formula f = new Formula();
+            Assert.AreEqual(0, f.GetVariables().Count);
+        }
+
+        [TestMethod]
+        public void ZeroArg3()
+        {
+            Formula f = new Formula();
+            Assert.AreEqual(0, f.Evaluate(s => 1));
+        }
+
+        [TestMethod]
+        public void ZeroArg4()
+        {
+            Formula f1 = new Formula();
+            Formula f2 = new Formula(f1.ToString());
+            Assert.AreEqual(0, f2.Evaluate(s => 1));
+        }
+
+        [TestMethod]
+        public void ThreeArg1()
+        {
+            Formula f = new Formula("x+y", s => s, s => true);
+            Assert.AreEqual(3, f.Evaluate(s => (s == "x") ? 1 : 2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void ThreeArg2()
+        {
+            Formula f = new Formula("x+y", s => "$", s => true);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void ThreeArg3()
+        {
+            Formula f = new Formula("x+y", s => s, s => false);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void ThreeArg4()
+        {
+            Formula f = new Formula("x+y", s => s == "x" ? "z" : s, s => s != "z");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormulaFormatException))]
+        public void ThreeArg5()
+        {
+            Formula f = new Formula("$", s => "x", s => true);
+        }
+
+        [TestMethod]
+        public void ThreeArg6()
+        {
+            Formula f = new Formula("1", s => "x", s => true);
+            Assert.AreEqual(1.0, f.Evaluate(s => { throw new UndefinedVariableException(""); }), 1e-6);
+        }
+
+        [TestMethod]
+        public void ThreeArg7()
+        {
+            Formula f = new Formula("y", s => "x", s => true);
+            Assert.AreEqual(1.0, f.Evaluate(s => (s == "x") ? 1 : 0), 1e-6);
+        }
+
+
+        [TestMethod]
+        public void ThreeArg8()
+        {
+            Formula f = new Formula("1e1 + e", s => "x", s => s == "x");
+            Assert.AreEqual(12.0, f.Evaluate(s => (s == "x") ? 2 : 0), 1e-6);
+        }
+
+        [TestMethod]
+        public void ThreeArg9()
+        {
+            Formula f = new Formula("xx+y", s => (s == "xx") ? "X" : "z", s => s.Length == 1);
+            Assert.AreEqual(10.0, f.Evaluate(s => (s == "X") ? 7 : 3), 1e-6);
+        }
+
+        [TestMethod]
+        public void ThreeArg10()
+        {
+            Formula f = new Formula("a + b + c + d", s => "x", s => true);
+            Assert.AreEqual(4.0, f.Evaluate(s => (s == "x") ? 1 : 0), 1e-6);
+        }
+
+        [TestMethod]
+        public void GetVars1()
+        {
+            Formula f = new Formula("0");
+            var expected = new HashSet<string>();
+            Assert.IsTrue(expected.SetEquals(f.GetVariables()));
+        }
+
+        [TestMethod]
+        public void GetVars2()
+        {
+            Formula f = new Formula("x");
+            var expected = new HashSet<string>();
+            expected.Add("x");
+            Assert.IsTrue(expected.SetEquals(f.GetVariables()));
+        }
+
+        [TestMethod]
+        public void GetVars3()
+        {
+            Formula f = new Formula("a * b - c + d / e * 2.5e6");
+            var expected = new HashSet<string>();
+            expected.Add("a");
+            expected.Add("b");
+            expected.Add("c");
+            expected.Add("d");
+            expected.Add("e");
+            var actual = f.GetVariables();
+            Assert.IsTrue(expected.SetEquals(actual));
+        }
+
+        [TestMethod]
+        public void GetVars4()
+        {
+            Formula f = new Formula("a * a + b * c - d * d");
+            var expected = new HashSet<string>();
+            expected.Add("a");
+            expected.Add("b");
+            expected.Add("c");
+            expected.Add("d");
+            var actual = f.GetVariables();
+            Assert.AreEqual(4, actual.Count);
+            Assert.IsTrue(expected.SetEquals(actual));
+        }
+
+        [TestMethod]
+        public void GetVars5()
+        {
+            Formula f = new Formula("x+y", s => s.ToUpper(), s => true);
+            var expected = new HashSet<string>();
+            expected.Add("X");
+            expected.Add("Y");
+            Assert.IsTrue(expected.SetEquals(f.GetVariables()));
+        }
+
+        [TestMethod]
+        public void GetVars6()
+        {
+            Formula f = new Formula("x+y+z", s => s + s, s => true);
+            var expected = new HashSet<string>();
+            expected.Add("xx");
+            expected.Add("yy");
+            expected.Add("zz");
+            Assert.IsTrue(expected.SetEquals(f.GetVariables()));
+        }
+
+        [TestMethod]
+        public void ToString1()
+        {
+            Formula f1 = new Formula("7");
+            Formula f2 = new Formula(f1.ToString());
+            Assert.AreEqual(7.0, f2.Evaluate(s => 0), 1e-6);
+        }
+
+        [TestMethod]
+        public void ToString2()
+        {
+            Formula f1 = new Formula("x");
+            Formula f2 = new Formula(f1.ToString());
+            Assert.AreEqual(8.0, f2.Evaluate(s => (s == "x") ? 8 : 0), 1e-6);
+        }
+
+        [TestMethod]
+        public void ToString3()
+        {
+            Formula f1 = new Formula("x", s => s.ToUpper(), s => true);
+            Formula f2 = new Formula(f1.ToString());
+            Assert.AreEqual(8.0, f2.Evaluate(s => (s == "X") ? 8 : 0), 1e-6);
+        }
+
+        [TestMethod]
+        public void ToString4()
+        {
+            Formula f1 = new Formula("a+b*(c-15)/2");
+            Formula f2 = new Formula(f1.ToString());
+            Assert.AreEqual(24.0, f2.Evaluate(s => char.IsLower(s[0]) ? 16 : 0), 1e-6);
+        }
+
+        [TestMethod]
+        public void ToString5()
+        {
+            Formula f1 = new Formula("a+b*(c-15)/2", s => s, s => true);
+            Formula f2 = new Formula(f1.ToString());
+            Assert.AreEqual(24.0, f2.Evaluate(s => char.IsLower(s[0]) ? 16 : 0), 1e-6);
+        }
+
+        [TestMethod]
+        public void ToString6()
+        {
+            Formula f1 = new Formula("a+b*(c-15)/2", s => s.ToUpper(), s => true);
+            Formula f2 = new Formula(f1.ToString());
+            Assert.AreEqual(24.0, f2.Evaluate(s => char.IsUpper(s[0]) ? 16 : 0), 1e-6);
         }
 
         /// <summary>
