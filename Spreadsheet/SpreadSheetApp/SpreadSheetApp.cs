6394218acf769a64fb676b364398461b7679df87
Tom Nguyen <nguyentom1996@yahoo.com>
2018-03-03 21:04:43 -0700

    I got something going

---
+++ Spreadsheet/SpreadSheetApp/SpreadSheetApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpreadSheetApp
+{
+	public partial class SpreadSheetApp : Form
+	{
+		public SpreadSheetApp()
+		{
+			InitializeComponent();
+		}
+
+		private void SpreadSheetApp_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
